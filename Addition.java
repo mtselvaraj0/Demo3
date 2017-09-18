cad4fd1ea215f1bd9c61869d2e07c73b44d495cd
kumar <manojkun@gmail.com>
2017-09-18 12:30:07 -0500

    Added another addition feature

--- Addition.java
+++ Addition.java
@@ -1,10 +1,12 @@
 class Man
 {
-	public static void main(string args[])
+	public static void main(String args[])
 	{
 		int a = 10;
 		int b = 20;
 		int c = a+b;
 		System.out.println(c);
+		int x = 2+3;
+		System.out.println(x);
 	}
 }
